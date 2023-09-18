666061c4e84b97b257a02048893abc30acc9fa0b
alizameller <alizameller@gmail.com>
2023-09-18 13:30:45 -0400

    fixed typos, data is loaded

--- hw3/cnn.py
+++ hw3/cnn.py
@@ -10,15 +10,17 @@ from mnist import MNIST
 mndata = MNIST('./')
 train_images, train_labels = mndata.load_training()
 train_images = tf.reshape(train_images, (60000, 28, 28))
-print(tf.shape(train_images))
-print(tf.shape(train_labels))
+# print(tf.shape(train_images))
+# print(tf.shape(train_labels))
 
 test_images, test_labels = mndata.load_testing()
-test_images = tf.reshape(test_images, (60000, 28, 28))
-print(tf.shape(test_images))
-print(tf.shape(test_labels))
+test_images = tf.reshape(test_images, (10000, 28, 28))
+# print(tf.shape(test_images))
+# print(tf.shape(test_labels))
 
+'''
 for i in range(9):  
     plt.subplot(330 + 1 + i)
-    plt.imshow(train_images[i], cmap=plt.get_cmap('gray'))
+    plt.imshow(test_images[i], cmap=plt.get_cmap('gray'))
 plt.show()
+'''
